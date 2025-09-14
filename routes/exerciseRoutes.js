6a03d7b9e4ada11290ce8be292813bdfcb124e14
pahmatug81042 <ptug81042@yahoo.com>
2025-09-14 20:53:00 +0000

    initial commit

--- routes/exerciseRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { searchExercises, searchVideos } = require("../controllers/exerciseController");
-const { protect } = require("../middleware/authMiddleware");
-
-router.get("/search", protect, searchExercises);
-router.get("/videos", protect, searchVideos);
-
-module.exports = router;
--- routes/youtubeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { searchVideos } = require("../controllers/youtubeController");
-const { protect } = require("../middleware/authMiddleware");
-
-// Protected route: only logged-in users can search
-router.get("/search", protect, searchVideos);
-
-module.exports = router;
